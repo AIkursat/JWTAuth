6c7040f1a287ed9d9b3f7f817cabdde3b44cb778
AIKursat <aynurkursat1999@hotmail.com>
2022-01-08 23:40:33 +0300

    Create the Models.

---
+++ AuthServer.Core/Models/UserApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    public class UserApp : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
