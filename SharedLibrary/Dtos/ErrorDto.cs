9314efd7912a7ef7e336464100f1dddcaa4faa26
AIKursat <aynurkursat1999@hotmail.com>
2022-01-09 14:53:19 +0300

    Create Dtos File

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<String>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            // We setted all in here.
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<String> errors, bool isShow)
+        {
+            // for overload.
+            Errors=errors;
+            IsShow = isShow;
+        }
+
+    }
+}
