e26477a06df90492d3657264f6aee1b18603d002
AIKursat <aynurkursat1999@hotmail.com>
2022-01-10 12:21:33 +0300

    Add IUserInterface

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+
+        //Find user by their username in the db.
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
