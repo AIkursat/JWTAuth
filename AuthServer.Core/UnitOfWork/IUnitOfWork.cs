de722f9d5597cf813e93166f404f1fb16a4f419b
AIKursat <aynurkursat1999@hotmail.com>
2022-01-10 17:25:34 +0300

    Create the AppDbSet

--- AuthServer.Core/UnitOfWork/IUnitOfWork.cs
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -8,6 +8,7 @@ namespace AuthServer.Core.UnitOfWork
 {
     public interface IUnitOfWork
     {
+
         Task CommitAsync();
 
         void Commit();
---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<UserRefreshToken> userRefreshTokens { get; set; }
+
+
+}
+}
