d8aa55bd163664a87c9fea5bcc045d003e30bfed
AIKursat <aynurkursat1999@hotmail.com>
2022-01-11 23:21:35 +0300

    Add the AutoMapper

---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper 
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+
+            return config.CreateMapper();
+
+        });
+        
+        public static IMapper Mapper => lazy.Value; // As long as you don't call the ObjectMapper Mapper. Codes in upside won't add the memory.
+
+    }
+}
